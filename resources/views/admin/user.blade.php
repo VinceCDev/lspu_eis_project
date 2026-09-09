daeea0218a93b97891afdeb7a28b5a6300f2b4aa
Vince Allen Cristal <vinceallencristal@gmail.com>
2026-09-09 13:50:47 +0800

    Fix Accounts action dropdown: position at button + match alumni width

--- resources/views/admin/user.blade.php
+++ resources/views/admin/user.blade.php
@@ -74,7 +74,7 @@
                                 <i class="fas fa-ellipsis-h"></i>
                             </button>
                             <teleport to="body">
-                                <div v-if="actionDropdown === account.user_id" class="teleported-action-dropdown fixed w-44 rounded-md shadow-lg bg-white dark:bg-gray-700 ring-1 ring-black ring-opacity-5 z-[300]" :style="{ top: dropdownPosition.top + 'px', left: dropdownPosition.left + 'px' }">
+                                <div v-if="actionDropdown === account.user_id" class="teleported-action-dropdown fixed w-32 rounded-md shadow-lg bg-white dark:bg-gray-700 ring-1 ring-black ring-opacity-5 z-[300]" :style="{ top: dropdownPosition.top + 'px', left: dropdownPosition.left + 'px' }">
                                     <div class="py-1" @click="actionDropdown = null">
                                         <a href="#" role="button" class="block px-4 py-2 text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-600" @click.prevent="viewAccount(account)"><i class="fas fa-eye mr-2"></i>View</a>
                                         <template v-if="account.user_role !== 'superadmin'">
